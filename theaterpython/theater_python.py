275069a6f8cd9daa78e6a3925d93eda2fb3c3555
Ana Luiza Fretta Barros <analufretta@gmail.com>
2025-03-11 11:20:04 +0100

    Alterations:
    variables x.y.i changed to self-explanatory names: agenda, lineup, lineup_dates

--- theaterpython/theater_python.py
+++ theaterpython/theater_python.py
@@ -9,7 +9,7 @@ performances = ["Lost in repetition", "The poetics of surviving"] #range of perf
 
 base = datetime.date.today()
 numdays = int(3)
-date_list = [(base + datetime.timedelta(days=x)).strftime("%d %B, %Y") for x in range(numdays)] #range of dates
+date_list = [(base + datetime.timedelta(days=agenda)).strftime("%d %B, %Y") for agenda in range(numdays)] #range of dates
 
 ticket = 35.50 #ticket price
 
@@ -24,8 +24,8 @@ print("Hello, {0}, welcome to Theater Python!\n".format(name))
 #Performance selection loop_1
 while True:
   print("Which performance would you would like to purchase?\n")
-  for y in range(len(performances)): #loop to print the whole varible performances
-    print("{0}. {1}".format(y + 1, performances[y])) #Formating
+  for lineup in range(len(performances)): #loop to print the whole varible performances
+    print("{0}. {1}".format(lineup + 1, performances[lineup])) #Formating
   else:
     perf_question = int(input("\nType the option number you would like to purchase: "))
 
@@ -35,15 +35,15 @@ while True:
     os.system("clear")
     continue
   else:
-    print("\nYou chose to buy tickets for '{0}'".format(performances[perf_question-1]))
-    input("Press any key to continue...")
-    os.system("clear")
+      print("\nYou chose to buy tickets for '{0}'".format(performances[perf_question-1]))
+      input("Press any key to continue...")
+      os.system("clear")
 
 #Date selection loop_2
   while True:
     print("When do you want to see '{0}'?\n".format(performances[perf_question-1]))
-    for i in range(len(date_list)): #loop to print the whole varible performances
-      print("{0}. {1}".format(i + 1, date_list[i])) #Formating
+    for lineup_dates in range(len(date_list)): #loop to print the whole varible performances
+      print("{0}. {1}".format(lineup_dates + 1, date_list[lineup_dates])) #Formating
     else:
       date_question = int(input("\nType the option number you would like to purchase: "))
 
@@ -59,18 +59,12 @@ while True:
 #Tickets loop_3
     while True:
       qtd = int(input("How many tickets would you like to purchase? ")) #quantity of tickets
-      if qtd > 5:
-         print("\nInvalid number. You can only buy a maximum of 5 tickets. Try again.")
+      if qtd > 5 or qtd < 1:
+         print("\nInvalid number. You can buy between 1 and 5 tickets. Try again.")
          input("Press any key to continue...")
          os.system("clear")
          print("You chose to buy tickets for '{0}' on '{1}'\n".format(performances[perf_question-1], date_list[date_question-1]))
          continue
-      elif qtd < 1:
-        print("\nInvalid number. You must buy at least 1 ticket. Try again.")
-        input("Press any key to continue...")
-        os.system("clear")
-        print("You chose to buy tickets for '{0}' on '{1}'\n".format(performances[perf_question-1], date_list[date_question-1]))
-        continue
       else:
         print("\nThe total price is €", (qtd * ticket))
         break #Finish loop_3
